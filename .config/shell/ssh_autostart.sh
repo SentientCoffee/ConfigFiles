b8f0241456e1d34692aee9df12da039c04e8956c
Daniel <22991229+SentientCoffee@users.noreply.github.com>
2022-09-13 22:25:37 -0400

    [Fix] SSH Windows integration

--- .config/shell/ssh_autostart.sh
+++ .config/shell/ssh_autostart.sh
@@ -15,10 +15,16 @@ AGENT_RUN_STATE=$(ssh-add -l >| /dev/null 2>&1; echo $?)
 if [ ! "${SSH_AUTH_SOCK}" ] || [ ${AGENT_RUN_STATE} = 2 ]; then
     echo -e "===== Starting ssh-agent ====="
     agent_start
+    echo -e "Agent started."
 
     echo -e "===== Adding keys ====="
     ssh-add ${HOME}/.ssh/id_ed25519
     ssh-add ${HOME}/.ssh/id_ed25519_ontariotech
+
+    echo -e "===== Setting Windows SSH environment variables ====="
+    echo -e "PID: ${SSH_AGENT_PID} | Socket: ${SSH_AUTH_SOCK})"
+    setx SSH_AGENT_PID ${SSH_AGENT_PID}
+    setx SSH_AUTH_SOCK ${SSH_AUTH_SOCK}
 elif [ "${SSH_AUTH_SOCK}" ] && [ ${AGENT_RUN_STATE} = 1 ]; then
     echo -e "ssh-agent already started.\n===== Adding keys ====="
     ssh-add ${HOME}/.ssh/id_ed25519
