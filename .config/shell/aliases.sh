a27880be0a52a39d59e35dda6795012e7ea50583
Daniel <22991229+SentientCoffee@users.noreply.github.com>
2022-09-21 18:10:05 -0400

    [Update] Many updated configs
    - Removed lsd (doesn't work well on Windows)
    - Modified bash aliases to remove linux-specific ones
    - Updated PowerToys configs and delete noisy files
    - Add more useful registry edits
    - Update VSCode and ShareX configs

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -4,16 +4,15 @@ alias cat="bat"
 alias chmod="sudo chmod -cR"
 alias chown="sudo chown -cR"
 alias cls="clear"
+alias code="code -r"
 alias codium="codium -r"
 alias cp="cp -iv"
 
 alias df="df -h"
-alias dmesg="sudo dmesg"
 alias du="du -h"
 
 alias egrep="grep -E --color=auto"
 
-alias fc-cache="fc-cache -fv"
 alias fgrep="grep -F --color=auto"
 
 alias ga="git add -v"
@@ -37,32 +36,21 @@ alias ka="killall -v"
 
 alias lg="lazygit"
 alias lg-dotfiles="lazygit -g \"${HOME}/.dotfiles\" -w \"${HOME}\""
-alias ll="lsd -Ahl --color=auto --group-directories-first"
+alias ll="ls -Ahl --color=auto --group-directories-first"
 alias ln="ln -iv"
 alias locate="locate -d ${UPDATEDB_DIR}/locate.db"
-alias ls="lsd -F --color=auto --group-directories-first"
+alias ls="ls -F --color=auto --group-directories-first"
 
 alias mv="mv -iv"
 alias mkdir="mkdir -pv"
-alias mkinitcpio="sudo mkinitcpio"
 
-alias nft="sudo nft"
-alias nvidia-settings="nvidia-settings --config=\"${XDG_CONFIG_HOME}/nvidia/settings\""
-
-# alias reboot="sudo reboot"
 alias rm="rm -iv"
 alias rmdir="rmdir -v"
-
-alias shutdown="sudo shutdown now"
 
 alias updatedb="mkdir -p ${UPDATEDB_DIR} ; updatedb --output=${UPDATEDB_DIR}/locate.db"
 alias usermod="sudo usermod"
 
-alias winetricks="winetricks -q"
-
 alias xargs-I="xargs -I {} "
-
-alias yarn="yarn --use-yarnrc \"${XDG_CONFIG_HOME}/yarn/config\""
 
 [ -n "${ZSH_VERSION}"  ] && alias refresh="source ${XDG_CONFIG_HOME}/zsh/.zshrc ; rehash ; updatedb"
 [ -n "${BASH_VERSION}" ] && alias refresh="source ${HOME}/.bashrc ; updatedb"
