b4700cf36a0f96e692d53befdc1bd9898041e322
Daniel <22991229+SentientCoffee@users.noreply.github.com>
2022-09-27 21:51:00 -0400

    [Update] Aliases, various application settings

--- bin/aliases.bat
+++ bin/aliases.bat
@@ -1,19 +1,12 @@
 @echo off
 
-:: --------------------------------------------------
-:: Directory aliases
-:: --------------------------------------------------
-
-subst W: %UserProfile%\Repos\WORK
-
-:: --------------------------------------------------
-:: Command aliases
-:: --------------------------------------------------
-
 doskey cat=%UserProfile%\bin\bat\bat.exe $*
 doskey cd=cd /d $*
 
+doskey lg=%UserProfile%\bin\lazygit\lazygit $*
+doskey ll=dir /n /oG $*
 doskey ls=dir /w /oG $*
 doskey ls1=dir /b /oG $*
-doskey ll=dir /n /oG $*
+
+doskey mkcd=mkdir $* $T chdir /d $*
 
--- bin/ssh_autologin.bat
+++ bin/ssh_autologin.bat
@@ -1,3 +1,4 @@
 @echo off
 
+subst W: %UserProfile%\Repos\WORK
 cmd.exe /k ""C:\Program Files\Git\bin\bash.exe" -c "${HOME}/.config/shell/ssh_autostart.sh""
