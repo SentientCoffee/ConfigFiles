8e08f0cc479634874edd3959f301315e4237ede0
Daniel <22991229+SentientCoffee@users.noreply.github.com>
2022-11-07 17:45:27 -0500

    [Update] Utility batch scripts

--- bin/cmd_autorun.bat
+++ bin/cmd_autorun.bat
@@ -6,27 +6,20 @@
 chcp 65001 >nul
 
 :: ---------------------------------------
-:: Custom prompt
-:: ---------------------------------------
-call %UserProfile%\bin\cmd_prompt.bat
-
-:: ---------------------------------------
 :: Aliases
 :: ---------------------------------------
 doskey cat=%UserProfile%\bin\bat\bat.exe $*
 doskey cd=cd /d $*
-doskey cd~=cd /d %UserProfile%\$*
-
-doskey git-dotfiles=git --git-dir="%UserProfile%/.dotfiles" --work-tree="%UserProfile%" $*
 
 doskey lg=%UserProfile%\bin\lazygit\lazygit $*
-doskey lg-dotfiles=%UserProfile%\bin\lazygit\lazygit -g "%UserProfile%/.dotfiles" -w "%UserProfile%" $*
 doskey ll=dir /n /oG $*
 doskey ls=dir /w /oG $*
-doskey ls-1=dir /b /oG $*
+doskey ls1=dir /b /oG $*
+
+doskey ka=for /f "tokens=2" %%p in ('tasklist /nh ^^^| findstr /i "$*"') do @(taskkill /f /pid %%p)
 
 doskey mkcd=mkdir $* $T chdir /d $*
 
 doskey refresh=%UserProfile%\bin\refresh_env.bat $T %UserProfile%\bin\cmd_autorun.bat
 
-doskey ssh-dbs=ssh dbs-mkm-03
+doskey ssh-dbs="C:\Program Files\Git\usr\bin\ssh" dbs-mkm-03
--- bin/cmd_login.bat
+++ bin/cmd_login.bat
@@ -1,5 +1,7 @@
 @echo off
 
+echo ---------- Setting up environment ----------
+
 :: ---------------------------------------
 :: Directory aliases
 :: ---------------------------------------
@@ -9,16 +11,16 @@ subst W: %UserProfile%\Repos\WORK >nul
 :: Custom prompt
 :: ---------------------------------------
 call %UserProfile%\bin\cmd_prompt.bat
-setx PROMPT %CUSTOM_PROMPT%
+
+:: ---------------------------------------
+:: Start ssh-agent
+:: ---------------------------------------
+:: echo ---------- Starting ssh-agent ----------
+:: call %UserProfile%\bin\start_ssh_agent.bat
+call "C:\Program Files\Git\usr\bin\bash.exe" -c "${HOME}/.config/shell/ssh_autostart.sh"
 
 :: ---------------------------------------
 :: Refresh environment variables (just in case)
 :: ---------------------------------------
 echo ---------- Refreshing environment variables ----------
 call %UserProfile%\bin\refresh_env.bat
-
-:: ---------------------------------------
-:: Start ssh-agent
-:: ---------------------------------------
-echo ---------- Starting ssh-agent ----------
-%UserProfile%\bin\start_ssh_agent.bat
